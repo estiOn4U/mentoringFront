e88b81de569f955293890337f514f7ea6b62a98d
estiOn4U <esti@on4u.es>
2021-02-05 10:28:45 +0100

    [Fix] Toggle collapse, remove event listener

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 window.addEventListener("load", init);
+window.addEventListener("resize", checkCollapse);
 
 function init() {
     let dialog = document.querySelector("#modal");
@@ -6,41 +7,49 @@ function init() {
     let closeButton = document.querySelector("#close-button");
     let openButton = document.querySelector("#open-button");
 
-    let clientWidth  = document.documentElement.clientWidth;
-
-
-    closeButton.addEventListener("click", function() {
+    closeButton.addEventListener("click", function () {
         dialog.classList.toggle("closed");
         dialog.setAttribute("aria-hidden", "true");
         dialog.removeAttribute("aria-modal");
         overlay.classList.toggle("closed");
     });
 
-    openButton.addEventListener("click", function() {
+    openButton.addEventListener("click", function () {
         dialog.classList.toggle("closed");
         dialog.setAttribute("aria-modal", "true");
         dialog.removeAttribute("aria-hidden");
         overlay.classList.toggle("closed");
     });
 
-    if(clientWidth <= 768) {
-        let acc = document.getElementsByClassName("accordion");
-        let i;
+    checkCollapse();
+}
 
-        for (i = 0; i < acc.length; i++) {
-            acc[i].addEventListener("click", function() {
-                this.classList.toggle("active");
-                let panel = this.nextElementSibling;
-                if (panel.style.display === "block") {
-                    panel.style.display = "none";
-                    panel.removeAttribute("aria-expanded");
-                } else {
-                    panel.style.display = "block";
-                    panel.setAttribute("aria-expanded", "true");
-                }
-            });
+function checkCollapse() {
+    let clientWidth = document.documentElement.clientWidth;
+    let acc = document.getElementsByClassName("accordion");
+    let i;
+    for (i = 0; i < acc.length; i++) {
+        if (clientWidth <= 768) {
+            acc[i].addEventListener("click", toggleCollapse);
+        }
+        else {
+            acc[i].removeEventListener("click", toggleCollapse);
+            let panel = acc[i].nextElementSibling;
+            panel.style.display = "block";
         }
 
+    }
+}
+
+function toggleCollapse(){
+    this.classList.toggle("active");
+    let panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+        panel.style.display = "none";
+        panel.removeAttribute("aria-expanded");
+    } else {
+        panel.style.display = "block";
+        panel.setAttribute("aria-expanded", "true");
     }
 }
 
