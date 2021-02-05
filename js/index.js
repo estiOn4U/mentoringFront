ce9ff8302d74b52500aeea0a34b0dcc5c77959a6
estiOn4U <esti@on4u.es>
2021-02-05 10:05:05 +0100

    responsive on tablet

--- js/index.js
+++ js/index.js
@@ -24,19 +24,19 @@ function init() {
     });
 
     if(clientWidth <= 768) {
-        console.log(clientWidth);
-
-        var acc = document.getElementsByClassName("accordion");
-        var i;
+        let acc = document.getElementsByClassName("accordion");
+        let i;
 
         for (i = 0; i < acc.length; i++) {
             acc[i].addEventListener("click", function() {
                 this.classList.toggle("active");
-                var panel = this.nextElementSibling;
+                let panel = this.nextElementSibling;
                 if (panel.style.display === "block") {
                     panel.style.display = "none";
+                    panel.removeAttribute("aria-expanded");
                 } else {
                     panel.style.display = "block";
+                    panel.setAttribute("aria-expanded", "true");
                 }
             });
         }
