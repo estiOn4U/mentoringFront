3988cd307a2a4f2794a055389ffb3a80dc4d32f2
estiOn4U <esti@on4u.es>
2021-02-08 12:56:04 +0100

    [Feat] Responsive carousel

--- js/index.js
+++ js/index.js
@@ -59,14 +59,32 @@ function plusSlides(n) {
 }
 
 function showSlides(n) {
-    var slides = document.getElementsByClassName("carousel__item");
+    let slides = document.getElementsByClassName("carousel__item");
+    let clientWidth = document.documentElement.clientWidth;
 
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+    if (n > slides.length) {
+        slideIndex = 1;
     }
-    slides[slideIndex-1].style.display = "block";
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    if (clientWidth <= 1440) {
+        for (let i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slides[slideIndex - 1].style.display = "inline-block";
+    } else {
+        for (let i = 0; i < slides.length; i += 1) {
+            slides[i].style.display = "none";
+        }
+        slides[slideIndex - 1].style.display = "inline-block";
+        if (slides[slideIndex]) {
+            slides[slideIndex].style.display = "inline-block";
+        }
+        else {
+            slides[slideIndex - 2].style.display = "inline-block";
+        }
+    }
 }
 
 function checkCollapse() {
