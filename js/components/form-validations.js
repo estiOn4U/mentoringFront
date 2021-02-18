30b58754158580a97e7c9e729f720d3ad1e77f86
estiOn4U <esti@on4u.es>
2021-02-18 12:31:28 +0100

    [Fix] Change checkbox class on error

--- js/components/form-validations.js
+++ js/components/form-validations.js
@@ -2,7 +2,7 @@ function validateForm() {
     let fieldset = this.previousElementSibling;
     let checkbox = fieldset.querySelector('#politica-contacto')
     if (!checkbox.checked) {
-        checkbox.parentElement.style.backgroundColor = '#ffcccc';
+        checkbox.parentElement.classList.add('onError');
         checkbox.focus();
         return false;
     }
