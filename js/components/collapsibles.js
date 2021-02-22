2c082ff2c8c5b14eb1f0d4c2d1fad3bb2453c3a8
estiOn4U <esti@on4u.es>
2021-02-22 13:32:35 +0100

    [Fix] NAvbar styling

--- js/components/collapsibles.js
+++ js/components/collapsibles.js
@@ -63,6 +63,7 @@ function toggleCollapseNavbar() {
 function toggleCollapseColumn() {
     this.classList.toggle("active");
     let panel = this.lastElementChild;
+
     genericCollapse(panel);
 }
 
